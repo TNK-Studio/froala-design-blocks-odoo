e808d0c41d4cb96fc01ede636a090a406c65b2d1
ruter <ruter.lv@gmail.com>
2019-06-07 11:16:37 +0800

    Add froala content block snippets

--- froala_block_contents/__manifest__.py
+++ froala_block_contents/__manifest__.py
@@ -5,13 +5,12 @@
     'name': "Froala Design Blocks of Content",
     'description': "Froala Design Blocks of Content",
     'author': "Ruter, TNK Studio",
-    'license': "FOWDL",
     'website': "https://github.com/ruter/froala-design-blocks-odoo",
     'category': 'Theme/Corporate',
     'version': '12.0.1.0.0',
-    'depends': ['website', 'website_theme_install'],
+    'depends': ['froala_block_base'],
     'data': [
-        'views/templates.xml',
+        'views/snippets.xml',
     ],
     'application': False,
 }
